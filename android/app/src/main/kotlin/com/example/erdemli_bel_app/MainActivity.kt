61cf1bdd732b0d561f0d0e2d622e4d00901483bc
Abdullah Soyuöz <abdullahsoyuoz@gmail.com>
2022-06-08 00:36:21 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/erdemli_bel_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.erdemli_bel_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: AppStarter(),
+  ));
+}
+
+class AppStarter extends StatelessWidget {
+  const AppStarter({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
